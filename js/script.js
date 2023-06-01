ec71140bf58d702f293412720f08e543e0db0fee
michelecanini <studio.michelecanini@gmail.com>
2023-06-01 21:35:02 +0200

    [UPDATE] insert array

--- js/script.js
+++ js/script.js
@@ -6,3 +6,56 @@
 //[ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin']  
 //Ma la tipografia per stampare il tutto vuole che le mandiamo una lista di ospiti in cui ogni ospite sia un oggetto javascript che ha come attributi: nome del tavolo, nome dell'ospite e posto occupato.
 //Generiamo e stampiamo in console la lista per i segnaposto.
+
+let tavoloVip =[
+    {
+        nome_tavolo: 'Tavolo Vip',
+        nome_ospite: 'Brad Pitt',
+        posto_occupato: '1',
+    },
+    {
+        nome_tavolo: 'Tavolo Vip',
+        nome_ospite: 'Johnny Depp',
+        posto_occupato: '2',
+    },
+    {
+        nome_tavolo: 'Tavolo Vip',
+        nome_ospite: 'Lady Gaga',
+        posto_occupato: '3',
+    },
+    {
+        nome_tavolo: 'Tavolo Vip',
+        nome_ospite: 'Cristiano Ronaldo',
+        posto_occupato: '4',
+    },
+    {
+        nome_tavolo: 'Tavolo Vip',
+        nome_ospite: 'Georgina Rodriguez',
+        posto_occupato: '5',
+    },
+    {
+        nome_tavolo: 'Tavolo Vip',
+        nome_ospite: 'Chiara Ferragni',
+        posto_occupato: '6',
+    },
+    {
+        nome_tavolo: 'Tavolo Vip',
+        nome_ospite: 'Fedez',
+        posto_occupato: '6',
+    },
+    {
+        nome_tavolo: 'Tavolo Vip',
+        nome_ospite: 'George Clooney',
+        posto_occupato: '7',
+    },
+    {
+        nome_tavolo: 'Tavolo Vip',
+        nome_ospite: 'Amal Clooney',
+        posto_occupato: '8',
+    },
+    {
+        nome_tavolo: 'Tavolo Vip',
+        nome_ospite: 'Maneskin',
+        posto_occupato: '9,10,11,12',
+    },
+]
