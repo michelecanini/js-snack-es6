ca355210c977dd63edcb0c12e26f8084d2126034
michelecanini <studio.michelecanini@gmail.com>
2023-06-05 15:44:51 +0200

    [UPDATE] structure js end html

---
+++ snack_3/js/script.js
@@ -0,0 +1,3 @@
+"use strict";
+
+//PROBLEMA: Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare in console la bici con peso minore utilizzando destructuring e template literal
