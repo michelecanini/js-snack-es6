807de086b6997e50f64b003d02c86c3f1cfa3560
michelecanini <studio.michelecanini@gmail.com>
2023-06-05 21:27:43 +0200

    [UPDATE] insert array snack_4

--- snack_4/js/script.js
+++ snack_4/js/script.js
@@ -1,3 +1,108 @@
 "use strict";
 
 // PROBLEMA: Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l'unica proprietà da compilare, le altre saranno tutte settate a 0. Generare numeri random al posto degli 0 nelle proprietà: punti fatti e falli subiti. Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+const legaSerieA = [
+    {
+        nome: 'Napoli',
+        punti_fatti: 90,
+        falli_subiti: 12.3,
+    },
+    {
+        nome: 'Lazio',
+        punti_fatti: 74,
+        falli_subiti: 12.4,
+    },
+    {
+        nome: 'Inter',
+        punti_fatti: 72,
+        falli_subiti: 10.7,
+    },
+    {
+        nome: 'Milan',
+        punti_fatti: 70,
+        falli_subiti: 11.5,
+    },
+    {
+        nome: 'Atalanta',
+        punti_fatti: 64,
+        falli_subiti: 10.1,
+    },
+    {
+        nome: 'Roma',
+        punti_fatti: 63,
+        falli_subiti: 13.3,
+    },
+    {
+        nome: 'Juventus',
+        punti_fatti: 62,
+        falli_subiti: 10.8,
+    },
+    {
+        nome: 'Fiorentina',
+        punti_fatti: 56,
+        falli_subiti: 13.4,
+    },
+    {
+        nome: 'Bologna',
+        punti_fatti: 54,
+        falli_subiti: 11.2,
+    },
+    {
+        nome: 'Torino',
+        punti_fatti: 53,
+        falli_subiti: 10.1,
+    },
+    {
+        nome: 'Monza',
+        punti_fatti: 52,
+        falli_subiti: 13,
+    },
+    {
+        nome: 'Udinese',
+        punti_fatti: 46,
+        falli_subiti: 12.5,
+    },
+    {
+        nome: 'Sassuolo',
+        punti_fatti: 45,
+        falli_subiti: 11.7,
+    },
+    {
+        nome: 'Empoli',
+        punti_fatti: 43,
+        falli_subiti: 12.1,
+    },
+    {
+        nome: 'Salernitana',
+        punti_fatti: 42,
+        falli_subiti: 11.6,
+    },
+    {
+        nome: 'Lecce',
+        punti_fatti: 36,
+        falli_subiti: 11.6,
+    },
+    {
+        nome: 'Spezia',
+        punti_fatti: 31,
+        falli_subiti: 10,
+    },
+    {
+        nome: 'Verona',
+        punti_fatti: 31,
+        falli_subiti: 10.1,
+    },
+    {
+        nome: 'Cremonese',
+        punti_fatti: 27,
+        falli_subiti: 10.7,
+    },
+    {
+        nome: 'Sampdoria',
+        punti_fatti: 19,
+        falli_subiti: 13.5,
+    },
+]        
+console.log(legaSerieA)
+
