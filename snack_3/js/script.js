224aa62121959fe975edac20832f7d7bcab53d7d
michelecanini <studio.michelecanini@gmail.com>
2023-06-05 16:31:46 +0200

    [UPDATE] insert array bike

--- snack_3/js/script.js
+++ snack_3/js/script.js
@@ -1,3 +1,27 @@
 "use strict";
 
-//PROBLEMA: Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare in console la bici con peso minore utilizzando destructuring e template literal
+//PROBLEMA: Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare in console la bici con peso minore utilizzando destructuring e template literal.
+
+let biciDaCorsa = [
+    {
+        nome: 'AEROAD CFR',
+        peso: 8,
+    },
+    {
+        nome: 'VAN RYSEL',
+        peso: 9,
+    },
+    {
+        nome: 'TRIBAN DONNA',
+        peso: 7,
+    },
+    {
+        nome: 'BTWIN',
+        peso: 10,
+    },
+    {
+        nome: 'LOBITO CARBONIO',
+        peso: 6,
+    },
+]        
+console.log(biciDaCorsa)
