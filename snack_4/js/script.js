129457ea60e2dc504f2e212c57df3742c9913de9
michelecanini <studio.michelecanini@gmail.com>
2023-06-06 18:29:11 +0200

    [UPDATE] insert random n esercize snack_4 done

--- snack_4/js/script.js
+++ snack_4/js/script.js
@@ -2,6 +2,7 @@
 
 // PROBLEMA: Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l'unica proprietà da compilare, le altre saranno tutte settate a 0. Generare numeri random al posto degli 0 nelle proprietà: punti fatti e falli subiti. Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 
+
 // ARRAY PER DATI RANDOM LEGA SERIE A
 
 const legaSerieA = [
@@ -108,10 +109,10 @@ const legaSerieA = [
 ]        
 console.log(legaSerieA)
 
-// FUNZIONE DEI NUMERI CASUALI
-function getRndInterger(min, max) {
-    return Math.floor(Math.random() * (max, min + 1)) + min;
-}
+legaSerieA.forEach((element) => {
+    element.punti_fatti = Math.floor(Math.random() * 99) + 1;
+    element.falli_subiti = Math.floor(Math.random() * 14) + 1;
+});
 
 // DATI REALI LEGA SERIE A
 /*
